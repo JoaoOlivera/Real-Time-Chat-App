ff0df3049e7decbbf4a112b5ed774a0ed25bd4bf
JoaoOlivera <jvoferreira0w2@gmail.com>
2024-02-01 22:19:59 -0300

    Real-Time Chat App

---
+++ public/script.js
@@ -0,0 +1,23 @@
+const botaoEnviar = document.getElementById('enviar');
+const caixaMensagem = document.getElementById('texto');
+const chat = document.getElementById('mensagens');
+
+
+const socket = io();
+
+botaoEnviar.addEventListener('click',  () => {
+
+if(caixaMensagem.value !== ''){
+        socket.emit('nova mensagem', caixaMensagem.value);
+        caixaMensagem.value='';
+    }
+
+})
+
+socket.addEventListener('nova mensagem', (msg) => {
+
+    const elementoMensagem = document.createElement('li'); //<li></li>
+    elementoMensagem.textContent = msg; //<li>Test mesage</li>
+    elementoMensagem.classList.add('mensagem'); //<li class='mensagem'>Test mesage</li>
+    chat.appendChild(elementoMensagem); //div id='mensagens'><li class='mensagem'>Test mesage</li>
+})
